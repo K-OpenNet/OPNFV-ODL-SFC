5b77e6cdfc89a7ddc2639e894fe21d736a458a0c
sungbogo28 <sungbogo28@gmail.com>
2017-11-23 01:21:13 +0900

    Update SfcServiceFunctionLoadPathAwareSchedulerAPITest.java

--- odl-loadpathawareSFscheduler/SfcServiceFunctionLoadPathAwareSchedulerAPITest.java
+++ odl-loadpathawareSFscheduler/SfcServiceFunctionLoadPathAwareSchedulerAPITest.java
@@ -101,6 +101,7 @@ public class SfcServiceFunctionLoadPathAwareSchedulerAPITest extends AbstractDat
                 add("196.168.55.13");
                 add("196.168.55.14");
                 add("196.168.55.15");
+                add("196.168.55.16");
             }
         };
 
@@ -123,6 +124,7 @@ public class SfcServiceFunctionLoadPathAwareSchedulerAPITest extends AbstractDat
                 add("196.168.55.113");
                 add("196.168.55.114");
                 add("196.168.55.115");
+                add("196.168.55.116");
             }
         };
 
@@ -146,7 +148,8 @@ public class SfcServiceFunctionLoadPathAwareSchedulerAPITest extends AbstractDat
                 add(new SfName("nat_6"));
                 add(new SfName("fw_5"));
                 add(new SfName("fw_6"));
-                add(new SfName("dpi_5"))
+                add(new SfName("dpi_5"));
+                add(new SfName("fw_7"));
             }
         };
 
@@ -169,6 +172,7 @@ public class SfcServiceFunctionLoadPathAwareSchedulerAPITest extends AbstractDat
                 add(new SftTypeName("firewall"));
                 add(new SftTypeName("firewall"));
                 add(new SftTypeName("dpi"));
+                add(new SftTypeName("firewall"));
             }
         };
 
